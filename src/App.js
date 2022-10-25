9a921d7637633871323bb099dea8bb55842728f3
MiXu <michael.schindler894@gmail.com>
2022-10-25 15:33:17 +0200

    folder structure

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import './App.css';
+import News from './components/News/News'
+import Header from './components/Header/Header'
+import Navbar from './components/Navbar/Navbar'
+import Main from './components/Main/Main'
+import Footer from './components/Footer/Footer'
 
 function App() {
   return (
     <div className="App">
-     
+      <Header/>
+      <Navbar/>
+      <Main/>
+      <Footer/>
     </div>
   );
 }
