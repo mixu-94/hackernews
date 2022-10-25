a67e17aa32cb08d56fa42c398a7a4ed273ac968c
doda1603 <captainchaos53@protonmail.com>
2022-10-25 19:44:42 +0200

    Footer HTML/CSS erste Schritte

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import './Footer.css'
+
+
+const Footer = () => {
+  return (
+    <div>
+       <div className='footer'>
+         DDM HackerNews
+
+       </div>
+
+       <div className='impressum'>
+        Impressum
+        <button onClick='topFunction()' className='arrow-button'>
+            Back<span class='arrow'>
+
+            </span>
+        </button>
+       </div>
+
+         
+        
+    </div>
+  )
+}
+
+export default Footer
